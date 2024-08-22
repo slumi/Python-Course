f9c3f4b87d36c0055abac14bee3d9b3d93036fb1
slumi <scheilalumi@hotmail.com>
2024-08-21 22:16:01 -0300

    Update expenses file and add loan

--- expenses.py
+++ expenses.py
@@ -14,3 +14,16 @@ expenses = [10.50, 8, 5, 15, 20, 5, 3]
 total = sum(expenses)
 
 print("You spent $", total, sep = '')
+
+print("-----------------------------")
+
+total2 = 0
+expenses2 = []
+num_expenses = int(input("Enter # of expenses:"))
+
+for i in range(num_expenses):
+    expenses2.append(float(input("Enter an expense:")))
+
+total2 = sum(expenses2)
+
+print("You spent $", total2, sep='')
